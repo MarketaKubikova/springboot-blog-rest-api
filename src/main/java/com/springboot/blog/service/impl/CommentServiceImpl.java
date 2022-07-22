5a0a7a9e4b2e61d34b5644dad822b2e5c7b5af1e
Markéta Vošická <vosickamarketa@gmail.com>
2022-07-22 19:15:36 +0200

    creating comments implemented

---
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,54 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Comment;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.CommentDTO;
+import com.springboot.blog.repository.CommentRepository;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public CommentDTO createComment(long postId, CommentDTO commentDTO) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        Comment comment = mapToEntity(commentDTO);
+        comment.setPost(post);
+
+        Comment newComment = commentRepository.save(comment);
+
+        return mapToDTO(newComment);
+    }
+
+    private CommentDTO mapToDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setName(comment.getName());
+        commentDTO.setEmail(comment.getEmail());
+        commentDTO.setBody(comment.getBody());
+
+        return commentDTO;
+    }
+
+    private Comment mapToEntity(CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setId(commentDTO.getId());
+        comment.setName(commentDTO.getName());
+        comment.setEmail(commentDTO.getEmail());
+        comment.setBody(commentDTO.getBody());
+
+        return comment;
+    }
+}
